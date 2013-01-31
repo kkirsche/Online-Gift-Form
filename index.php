f51009362cb314a5c72375817d29aea16f1b64b6
Kevin Kirsche <kkirsche@hsc.edu>
2013-01-31 09:49:09 -0500

    Updated to jQuery 1.9.0 from 1.8.3

--- index.php
+++ index.php
@@ -737,8 +737,8 @@ $formKey = new formKey();
         </div><!-- End main container-->
 
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
         <script src="js/jquery.form.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
