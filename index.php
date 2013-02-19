c5e74ea3162d6c70c0c6334d63150d062e41a23f
Kevin Kirsche <kkirsche@hsc.edu>
2013-02-19 08:23:38 -0500

    Text addition

--- index.php
+++ index.php
@@ -713,7 +713,7 @@ $formKey = new formKey();
                             <div class="leftText footerText floatLeft">
                                 <p>
                                     <strong>Other Giving Methods:</strong><br />
-                                    If you would like to call us with your credit card information, you can contact us toll-free at <a href="tel:18008651776" title="Call us to donate">1&ndash;800&ndash;865&ndash;1776</a>. We are available Monday&ndash;Friday, 8:30 AM to 5:00 PM <abbr title="Eastern Standard Time">EST</abbr>.<br />
+                                    If you would like to call us with your credit card information, you can contact us toll-free at <a href="tel:18008651776" title="Call us to donate">1-800-865-1776</a>. We are available Monday&ndash;Friday, 8:30 AM to 5:00 PM <abbr title="Eastern Standard Time">EST</abbr>.<br />
                                     <hr />
                                     &copy; Hampden-Sydney College | 2012&ndash;2013<br />
                                     <a href="http://www.hsc.edu/Computing-Center/Policies/Digital-Copyright-Infringements.html" title="Copyright" target="_blank"><em>Copyright</em></a> | <a href="http://www.hsc.edu/Emergencies.html" title="Emergencies" target="_blank"><em>Emergencies</em></a> | <a href="http://www.hsc.edu/Search/A-Z-Index.html" title="Site Index" target="_blank"><em>Site Index</em></a>
