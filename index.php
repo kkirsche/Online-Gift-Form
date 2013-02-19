f081e311fa6e15bee4b2ca6e1fd1402a099290bd
Kevin Kirsche <kkirsche@hsc.edu>
2013-02-19 08:23:16 -0500

    Update jQuery to v1.9.1

--- index.php
+++ index.php
@@ -738,7 +738,7 @@ $formKey = new formKey();
 
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="js/jquery.form.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
